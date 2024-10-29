5a6f6dc8ee0ef9a2754ce8cd410073da479daccd
subhilesh-ps <subhileshps96@gmail.com>
2024-10-29 17:10:59 +0530

    auth ongoing

---
+++ server/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//static signup method
+
+userSchema.statics.signup = async function (email, password) {
+  //validation
+
+  if (!email || !password) {
+    throw Error("Please provide both email and password");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password must be at least 8 characters long, contain at least one uppercase an lowercause an  special chr"
+    );
+  }
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already exists");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const workoutRoutes = require("./routes/workouts");
-
+const userRoutes = require("./routes/user");
 //express app
 const app = express();
 
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 //routes
 
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose
